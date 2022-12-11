c93a310ae9aab2bd89b0510f76211f4bca74906d
Mike Harris <osiris2918@gmail.com>
2022-12-11 14:58:13 -0600

    Fix #8 The multiselect autocompletes now no longer display already selected values. There's a lesson here also that MudBlazor doesn't handle nullable value types well.

--- TimeTrack/Shared/ViewModels/ActivityOwner.cs
+++ TimeTrack/Shared/ViewModels/ActivityOwner.cs
@@ -1,8 +1,34 @@
 ﻿namespace TimeTrack.Shared.ViewModels
 {
-    public class ActivityOwner
+    public class ActivityOwner : IEquatable<ActivityOwner?>
     {
         public string Name { get; init; }
         public long Id { get; init; }
+
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as ActivityOwner);
+        }
+
+        public bool Equals(ActivityOwner? other)
+        {
+            return other is not null &&
+                   Id == other.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public static bool operator ==(ActivityOwner? left, ActivityOwner? right)
+        {
+            return EqualityComparer<ActivityOwner>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(ActivityOwner? left, ActivityOwner? right)
+        {
+            return !(left == right);
+        }
     }
 }
